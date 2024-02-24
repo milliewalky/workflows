7ff7449d658a009b11091a8b6de04db670045bb2
milliewalky <sovia@disroot.org>
2024-02-24 13:23:12 +0100

    style, again

--- build.bat
+++ build.bat
@@ -5,27 +5,27 @@ cd /D "%~dp0"
 :: --- Unpack Arguments -------------------------------------------------------
 for %%a in (%*) do      set "%%a=1"
 
-if not "%msvc%"=="1"    if not "%clang%"=="1" set msvc= 1
-if not "%release%"=="1"                       set debug=1
+if not "%msvc%"=="1" if not "%clang%"=="1" set msvc= 1
+if not "%release%"=="1" set debug=1
 
-if "%debug%"=="1"       set release=0 && echo [debug mode]
-if "%release%"=="1"     set debug=  0 && echo [release mode]
-if "%msvc%"=="1"        set clang=  0 && echo [msvc compile]
-if "%clang%"=="1"       set msvc=   0 && echo [clang compile]
+if "%debug%"=="1"   set release=0 && echo [debug mode]
+if "%release%"=="1" set debug=  0 && echo [release mode]
+if "%msvc%"=="1"    set clang=  0 && echo [msvc compile]
+if "%clang%"=="1"   set msvc=   0 && echo [clang compile]
 
-if "%~1"==""            echo [default mode, assuming `sample` build] && set sample=1
+if "%~1"=="" echo [default mode, assuming `sample` build] && set sample=1
 
 :: --- Compile/Link Line Definitions ------------------------------------------
-set cl_common=     /I..\src\ /I..\local\ /nologo /FC /Z7
-set clang_common=  -I..\src\ -I..\local\ -gcodeview -fdiagnostics-absolute-paths -Wall -Wno-unknown-warning-option -Wno-missing-braces -Wno-unused-function -Wno-writable-strings -Wno-unused-value -Wno-unused-variable -Wno-unused-local-typedef -Wno-deprecated-register -Wno-deprecated-declarations -Wno-unused-but-set-variable -Wno-single-bit-bitfield-constant-conversion -Xclang -flto-visibility-public-std -D_USE_MATH_DEFINES -Dstrdup=_strdup -Dgnu_printf=printf
-set cl_debug=      call cl /Od %cl_common%
-set cl_release=    call cl /O2 /DNDEBUG %cl_common%
-set clang_debug=   call clang -g -O0 %clang_common%
-set clang_release= call clang -g -O2 -DNDEBUG %clang_common%
-set cl_link=       /link /MANIFEST:EMBED /INCREMENTAL:NO
-set clang_link=    -fuse-ld=lld -Xlinker /MANIFEST:EMBED -Xlinker
-set cl_out=        /out:
-set clang_out=     -o
+set cl_common=    /I..\src\ /I..\local\ /nologo /FC /Z7
+set clang_common= -I..\src\ -I..\local\ -gcodeview -fdiagnostics-absolute-paths -Wall -Wno-unknown-warning-option -Wno-missing-braces -Wno-unused-function -Wno-writable-strings -Wno-unused-value -Wno-unused-variable -Wno-unused-local-typedef -Wno-deprecated-register -Wno-deprecated-declarations -Wno-unused-but-set-variable -Wno-single-bit-bitfield-constant-conversion -Xclang -flto-visibility-public-std -D_USE_MATH_DEFINES -Dstrdup=_strdup -Dgnu_printf=printf
+set cl_debug=     call cl /Od %cl_common%
+set cl_release=   call cl /O2 /DNDEBUG %cl_common%
+set clang_debug=  call clang -g -O0 %clang_common%
+set clang_release=call clang -g -O2 -DNDEBUG %clang_common%
+set cl_link=      /link /MANIFEST:EMBED /INCREMENTAL:NO
+set clang_link=   -fuse-ld=lld -Xlinker /MANIFEST:EMBED -Xlinker
+set cl_out=       /out:
+set clang_out=    -o
 
 :: --- Per-Build Settings -----------------------------------------------------
 if "%msvc%"=="1"  set only_compile=/c
